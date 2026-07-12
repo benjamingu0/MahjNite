d9efa7ad2403e49217e675d1dc4e21ba4fd2d47d
Benjamin Guo <bguo12345678@gmail.com>
2026-07-12 13:51:32 -0500

    Initial Commit

---
+++ MahjNite/MahjNiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MahjNiteApp.swift
+//  MahjNite
+//
+//  Created by Benjamin Guo on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct MahjNiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MahjNiteTests/MahjNiteTests.swift
@@ -0,0 +1,19 @@
+//
+//  MahjNiteTests.swift
+//  MahjNiteTests
+//
+//  Created by Benjamin Guo on 7/12/26.
+//
+
+import Testing
+@testable import MahjNite
+
+struct MahjNiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
