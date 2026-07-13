3d50e31ddbf1375d904f28dc1b5aaca8d478157a
Benjamin Guo <bguo12345678@gmail.com>
2026-07-13 15:28:00 -0500

    Defined tile and added initial wall creation - Defined suits (characters, bamboos, and circles) and honors (winds and dragons) - Added each tile 4 times to the initial wall + shuffled

--- MahjNite/Tile.swift
+++ MahjNite/Tile.swift
@@ -4,5 +4,29 @@
 //
 //  Created by Benjamin Guo on 7/13/26.
 //
+//  Tile defintion with suits and honors
+//
 
 import Foundation
+
+enum Suit: Equatable {
+    case characters, bamboos, circles
+}
+
+enum Wind: Equatable {
+    case east, south, west, north
+}
+
+enum Dragon: Equatable {
+    case red, green, white
+}
+
+enum Honor: Equatable {
+    case wind(Wind)
+    case dragon(Dragon)
+}
+
+enum Tile: Equatable {
+    case suit(suit: Suit, rank: Int)
+    case honor(honor: Honor)
+}
--- MahjNite/Wall.swift
+++ MahjNite/Wall.swift
@@ -4,5 +4,34 @@
 //
 //  Created by Benjamin Guo on 7/13/26.
 //
+//  Creates the tile wall
+//
 
 import Foundation
+
+func generateWall() -> [Tile] {
+    var tiles: [Tile] = []
+    let suits: [Suit] = [.characters, .bamboos, .circles]
+    let winds: [Wind] = [.east, .south, .west, .north]
+    let dragons: [Dragon] = [.red, .green, .white]
+    
+    for suit in suits {
+        for rank in 1...9 {
+            for i in 1...4 {
+                tiles.append(.suit(suit: suit, rank: rank))
+            }
+        }
+    }
+    for wind in winds {
+        for i in 1...4 {
+            tiles.append(.honor(honor: .wind(wind)))
+        }
+    }
+    for dragon in dragons {
+        for i in 1...4 {
+            tiles.append(.honor(honor: .dragon(dragon)))
+        }
+    }
+    
+    return tiles.shuffled()
+}
