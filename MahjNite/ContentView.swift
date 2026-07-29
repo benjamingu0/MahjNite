3254905098a43cb52b35c40f264c9db171f83887
Benjamin Guo <bguo12345678@gmail.com>
2026-07-29 13:59:50 -0500

    Started workingt on drawing tiles - Implemented wall - Encountered a strange bug where hand starts at 123 tiles, was unable to fix but will debug soon

--- MahjNite/ContentView.swift
+++ MahjNite/ContentView.swift
@@ -8,19 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var hand: [Tile] = generateWall().prefix(12).map { $0 }
+    @State private var hand: [Tile]
+    @State private var wall: [Tile]
     @State private var discards: [Tile] = []
+    
+    init() {
+        let fullWall = generateWall()
+        _hand = State(initialValue: Array(fullWall.prefix(13)))
+        _wall = State(initialValue: Array(fullWall.dropFirst(13)))
+    }
     
     var body: some View {
         VStack {
             Spacer()
             DiscardPileView(tiles: discards)
             Spacer()
-            Text("Your Hand")
+            Text("Your Hand (\(wall.count) tiles left in wall)")
                 .font(.headline)
-            HandView(tiles: hand, onTileTapped: { index in
+            HandView(tiles: hand, onTileTapped: {index in
                 let tile = hand.remove(at: index)
                 discards.append(tile)
+                if !wall.isEmpty {
+                    hand.append(wall.removeFirst())
+                }
             })
             .padding(.bottom)
         }
